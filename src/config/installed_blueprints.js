7e71f8a6fb0baf2cb180578429e4aa1836a15bf8
Crowdbotics <team@crowdbotics.com>
2020-10-30 09:42:12 +0000

    add package blueprint-react-native-sign-in-5-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'SignIn51165014', human_name: 'Sign In 5 1', access_route: 'SignIn51165014'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import SignIn51165014Navigator from '../features/SignIn51165014/navigator';
 
 /**
  * new navigators can be imported here
@@ -13,6 +14,7 @@ import SideMenu from './sideMenu';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+SignIn51165014: { screen: SignIn51165014Navigator },
 
     /** new navigators can be added here */
     SplashScreen: {
